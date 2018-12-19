ed191d6f08cf7b83c39013f8f84440d19e17987c
Mahesh Shivamallappa <mahesh.s.rails@gmail.com>
2018-12-19 23:59:42 +1100

    Solved Linear Regression with one variable

--- machine-learning-ex1/ex1/ex1.m
+++ machine-learning-ex1/ex1/ex1.m
@@ -66,14 +66,14 @@ computeCost(X, y, theta)
 theta = gradientDescent(X, y, theta, alpha, iterations);
 
 % print theta to screen
-fprintf('Theta found by gradient descent: ');
-fprintf('%f %f \n', theta(1), theta(2));
+fprintf('Theta found by gradient descent: '); 
+fprintf('%f %f \n', theta(1), theta(2)); 
 
 % Plot the linear fit
 hold on; % keep previous plot visible
 plot(X(:,2), X*theta, '-')
 legend('Training data', 'Linear regression')
-hold off % don't overlay any more plots on this figure
+hold off % dont overlay any more plots on this figure
 
 % Predict values for population sizes of 35,000 and 70,000
 predict1 = [1, 3.5] *theta;
--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -17,16 +17,16 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
+    theta = theta - (alpha / m) * (transpose(X) * (X * theta - y));
 
 
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
+
+    % fprintf('Cost Function J: '); 
+    % fprintf('%f \n', J_history(iter));
 
 end
 
