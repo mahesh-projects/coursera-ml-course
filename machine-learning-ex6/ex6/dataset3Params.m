00d9ead91d479753abc9ec1ee6873b3999874440
Mahesh Shivamallappa <mahesh.s.rails@gmail.com>
2019-01-13 13:23:11 +1100

    Completed Gaussian Kernel and Parameters (C, sigma) for Dataset 3 exercises

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -22,7 +22,32 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+C_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
+sigma_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
 
+%C_vec = [0.01 0.03]';
+%sigma_vec = [0.01 0.03]';
+results = [];
+
+%outer loop - iterate through C values
+for i = 1:length(C_vec)
+    C_test = C_vec(i);
+    %inner loop - iterate through sigma values
+    for j = 1:length(sigma_vec)
+        sigma_test = sigma_vec(j);
+        model = svmTrain(X, y, C_test, @(x1, x2) gaussianKernel(x1, x2, sigma_test)); 
+        %Compute the predictions for the validation set using svmPredict() with model and Xval.
+        predictions = svmPredict(model, Xval);
+        err_value = mean(double(predictions ~= yval));
+        results = [results; C_test sigma_test err_value];
+    end
+end
+
+[min_values, index_min_values] = min(results);
+
+%Pick C and sigma that corresponds to the minimum err_value for the validation set
+C = results(index_min_values(3), 1)
+sigma = results(index_min_values(3),2)
 
 
 
