506ed270fa8fab9a6ffa86d87a550958972b6c66
Mahesh Shivamallappa <mahesh.s.rails@gmail.com>
2019-01-13 16:44:46 +1100

    Email Feature Extraction

--- machine-learning-ex6/ex6/ex6_spam.m
+++ machine-learning-ex6/ex6/ex6_spam.m
@@ -136,3 +136,28 @@ p = svmPredict(model, x);
 fprintf('\nProcessed %s\n\nSpam Classification: %d\n', filename, p);
 fprintf('(1 indicates spam, 0 indicates not spam)\n\n');
 
+%% =================== Part 7: Optional Exercises - Try your own emails=====================
+% Set the file to be read in (change this to spamSample3.txt - Actual Spam email
+filename = 'spamSample3.txt';
+
+% Read and predict
+file_contents = readFile(filename);
+word_indices  = processEmail(file_contents);
+x             = emailFeatures(word_indices);
+p = svmPredict(model, x);
+
+fprintf('\nProcessed %s\n\nSpam Classification: %d\n', filename, p);
+fprintf('(1 indicates spam, 0 indicates not spam)\n\n');
+
+% Now an email classified as NOT spam
+filename = 'emailSample3.txt';
+
+% Read and predict
+file_contents = readFile(filename);
+word_indices  = processEmail(file_contents);
+x             = emailFeatures(word_indices);
+p = svmPredict(model, x);
+
+fprintf('\nProcessed %s\n\nSpam Classification: %d\n', filename, p);
+fprintf('(1 indicates spam, 0 indicates not spam)\n\n');
+
